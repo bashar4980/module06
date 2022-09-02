24cc08bab635de86dd22ac2709a868d07ccdc574
bashar4980 <sagor.ahmed3669@gmail.com>
2022-09-02 12:06:13 +0600

    second problem is done

--- problem01.js
+++ problem01.js
@@ -3,7 +3,7 @@
 const person = {
     playCricket: true ,
     playFoodball : false ,
-    educationInstitute :['kbh' , 'NimtolaCollege' , 'Islamic University'],
+    educationInstitute :['kbh' , 'Nimtola College' , 'Islamic University'],
     biodata:{
         name: "Bashar Ahmed",
         age: 24 ,
@@ -20,3 +20,10 @@ const person = {
 // const bio = person.fullName() ;
 // console.log(bio)
 // ২. একটা টেম্পলেট স্ট্রিং দিয়ে একটা স্ট্রিং ভেরিয়েবল ডিক্লেয়ার করো। সেটার মধ্যে উপরের অবজেক্ট এর একটা তিনটা প্রপার্টি এর মান তোমার টেমপ্লেট স্ট্রিং এর মধ্যে বসাও। এর মধ্যে একটা প্রপার্টি সেট করবে--যেটা নেস্টেড অবজেক্ট আছে সেটার প্রপার্টি থেকে। আরেকটা প্রপার্টি হবে যে প্রপার্টি এর মান একটা array সেটার সেকেন্ড পজিশন এর উপাদান দিয়ে। আর বাকি একটা তোমার ইচ্ছা মতো বসাতে পারো। 
+
+
+const userBio = `My name is ${person.biodata.name}. I read in ${person.educationInstitute[01]} . I love play ${person.playCricket}` ;
+// console.log(userBio);
+
+// শূন্য প্যারামিটার ওয়ালা একটা অ্যারো ফাংশন লিখবে যেটা ৮৯ রিটার্ন করবে। 
+
